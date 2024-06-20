9e0f46c6ff8687173bfe7ad111a11f20fbab344f
Gabriel Abreu <gabriel.abreu2007@hotmail.com>
2024-06-20 18:03:38 -0300

    q

--- estrutura_decisao/exe_006.py
+++ estrutura_decisao/exe_006.py
@@ -1,2 +1,21 @@
 '''Faça um Programa que leia três números e mostre o maior deles'''
 
+from cabecalho import linha
+linha(nome_programa='Qual o maior número')
+
+num_1= int(input('Informe o 1º número: '))
+num_2= int(input('Informe o 2º número: '))
+num_3= int(input('Informe o 3º número: '))
+
+
+
+if num_1 > num_2 and num_1 > num_3:
+    print(f'{num_1} foi o maior número informado')
+
+elif num_2 > num_3:
+
+    print(f'{num_2} foi o maior número informado')
+
+else:
+    print(f'{num_3} foi o maior número informado')
+    
