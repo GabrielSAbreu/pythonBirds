aa85db789e9b1b6c4962b951bf0b8a26a9eadb87
Gabriel Abreu <gabriel.abreu2007@hotmail.com>
2024-06-21 16:51:49 -0300

    wip: resolvendo exercicio 09

--- estrutura_decisao/exe_008.py
+++ estrutura_decisao/exe_008.py
@@ -7,3 +7,13 @@ produto_1 = float(input('Informe o preço do 1º produto R$: '))
 produto_2 = float(input('Informe o preço do 2º produto R$: '))
 produto_3 = float(input('Informe o preço do 3º produto R$: '))
 
+menor = float
+
+if produto_1 < produto_2 and produto_2 < produto_3:
+    menor = produto_1
+elif produto_2 < produto_3:
+    menor = produto_2
+else:
+    menor = produto_3
+
+print(f'O produto mais barato é o  que custa R$ {menor:.2f}')
---
+++ estrutura_decisao/exe_009.py
@@ -0,0 +1,5 @@
+'''Faça um Programa que leia três números e mostre-os em ordem decrescente. '''
+
+n1 = 5
+n2 = 8
+n3 = 6
