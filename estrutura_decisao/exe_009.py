eb838a45d58b41d5d98735b15ea0ddd8c85d37c5
Gabriel Abreu <87873067+GabrielSAbreu@users.noreply.github.com>
2024-06-22 03:32:17 +0000

    wip: exercicio 10 estruturas de decisao

--- estrutura_decisao/exe_009.py
+++ estrutura_decisao/exe_009.py
@@ -1,5 +1,31 @@
 '''Faça um Programa que leia três números e mostre-os em ordem decrescente. '''
 
-n1 = 5
-n2 = 8
-n3 = 6
+n1 = 2
+n2 = 1
+n3 = 3
+'''podemos ter os seguintes cenários:
+n1 > n2 > n3
+n1 > n3 > n2
+n2 n3 n1
+n2 n1 n3
+n3 n2 n1
+n3 n1 n2
+'''
+if n1 > n2 > n3:#A
+    print(n1,n2,n3)
+    print('A')
+elif n1 > n2 < n3 < n1:#B
+    print(n1,n3,n2)
+    print('B')
+elif n1 < n2 > n3 > n1:#C
+    print(n2,n3,n1)
+    print('C')
+elif n1 < n2 > n3 < n1:#D
+    print(n2,n1,n3)
+    print('D')
+elif n1 < n2 < n3:#E
+    print(n3,n2,n1)
+    print('E')
+else:
+    print(n3,n1,n2)
+    print('F')
