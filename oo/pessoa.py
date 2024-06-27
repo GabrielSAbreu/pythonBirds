3c20de1b2c60adf5609c529fefc5b6f9c5f02999
Gabriel Abreu <gabriel.abreu2007@hotmail.com>
2024-06-27 17:39:14 -0300

    objetos como atributo

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,15 +1,29 @@
 class Pessoa:
-    def __init__(self, nome = None, idade = 38):
+    def __init__(self, *filhos,  nome = None, idade = 38):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
        
     def cumprimentar(self):
         return 'Olá'
     
 
 if __name__ == '__main__':
-    p = Pessoa('Gabriel', 40)
-    print(Pessoa.cumprimentar(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    print(p.idade)
+    aurora = Pessoa(nome='Aurora')
+    gabriel = Pessoa(aurora, nome = 'Gabriel')
+    print(Pessoa.cumprimentar(gabriel))
+    print(gabriel.cumprimentar())
+    print(gabriel.nome)
+    print(gabriel.idade)
+    for filho in gabriel.filhos:
+        print(filho.nome)
+
+'''if __name__ == '__main__':
+    aurora = Pessoa(nome='Aurora')
+    gabriel = Pessoa(aurora, nome='Gabriel')
+    print(Pessoa.cumprimentar(gabriel))
+    print(gabriel.cumprimentar())
+    print(gabriel.nome)
+    print(gabriel.idade)
+    for filho in gabriel.filhos:
+        print(filho.nome)'''
