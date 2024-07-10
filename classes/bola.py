d56a9a74b57ed66974daf6924711f6dd6db1cc96
Gabriel Abreu <87873067+GabrielSAbreu@users.noreply.github.com>
2024-07-10 03:10:10 +0000

    wip:adicionado atributos e metodos da classe bola, exercicio 1 da lista de exercícios sobre classes em python

---
+++ classes/bola.py
@@ -0,0 +1,18 @@
+class Bola:
+    def __init__(self, cor, circunferencia, material):
+        self.cor = cor
+        self.circunferencia = circunferencia
+        self.material = material
+        
+    #Declaração dos métodos
+
+    def trocaCor(self):
+        novaCor = input('Informe uma nova cor:')
+        return novaCor
+
+    def mostraCor(self):
+        print(f'A antiga cor era {self.cor} agora a nova cor é {bola.trocaCor()}')
+    
+bola  = Bola('rosa' , 15 , 'plastico')
+
+bola.mostraCor()
